e049d2674a1c365e34accd161ed62370dd4bd076
melon <ememendeztabilo@gmail.com>
2020-01-27 19:18:35 -0300

    initial commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html class="wide wow-animation" lang="en">
+  @include('partials.head')
+  <body>
+    <div class="ie-panel"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="/images/ie8-panel/warning_bar_0000_us.jpg" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    <div class="preloader" id="loading">
+      <div class="preloader-body">
+        <div id="loading-center-object">
+          <div class="object" id="object_four"></div>
+          <div class="object" id="object_three"></div>
+          <div class="object" id="object_two"></div>
+          <div class="object" id="object_one"></div>
+        </div>
+      </div>
+    </div>
+    <div class="page">
+     @include('partials.header');
+      @yield('content')
+      @include('partials.footer');
+
+    </div>
+    <div class="snackbars" id="form-output-global"></div>
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script> 
+  </body>
+</html>
+
+
+
+
+
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,52 @@
+<!-- Page Footer-->
+<a class="banner" href="https://www.templatemonster.com/website-templates/monstroid2.html" target="_blank"><img src="/images/monstroid_big.jpg" alt="" height="0"/></a>
+<footer class="section footer-classic">
+  <div class="footer-classic-main">
+    <div class="container">
+      <div class="row row-50 justify-content-lg-between">
+        <div class="col-sm-7 col-lg-3 col-xl-2"><a class="brand" href="index.html"><img src="/images/logo-default-399x82.png" alt="" width="199" height="41"/></a>
+          <p><span style="max-width: 250px;">Our agency provides quality digital marketing services.</span></p><a class="button button-sm button-default-outline button-winona" href="#">Get in Touch</a>
+        </div>
+        <div class="col-sm-5 col-lg-3 col-xl-2">
+          <h4 class="footer-classic-title">About</h4>
+          <ul class="list footer-classic-list">
+            <li><a href="about-us.html">About Us</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Press and media</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-7 col-lg-5 col-xl-3">
+          <h4 class="footer-classic-title">Quick Links</h4>
+          <ul class="list footer-classic-list footer-classic-list_2-cols">
+            <li><a href="#">Sign Up</a></li>
+            <li><a href="#">Our Team</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Pricing</a></li>
+            <li><a href="#">Testimonials</a></li>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Services</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-5 col-lg-9 col-xl-2">
+          <h4 class="footer-classic-title">Contact Us</h4>
+          <div class="row row-20 row-sm-35">
+            <div class="col-6 col-sm-12 col-lg-8 col-xl-12">
+              <div class="row row-10 text-default">
+                <div class="col-lg-6 col-xl-12"><a href="mailto:#">info@demolink.org</a></div>
+                <div class="col-lg-6 col-xl-12"><a class="big" href="tel:#">+1-853-234-65</a></div>
+              </div>
+            </div>
+            <div class="col-6 col-sm-12 col-lg-4 col-xl-12 text-right text-sm-left">
+              <div class="group group-xs"><a class="link link-social-1 mdi mdi-twitter" href="#"></a><a class="link link-social-1 mdi mdi-facebook" href="#"></a><a class="link link-social-1 mdi mdi-instagram" href="#"></a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-classic-aside">
+    <div class="container">
+      <p class="rights"><span>&copy;&nbsp;</span><span class="copyright-year"></span>. All Rights Reserved. Design by <a href="https://www.templatemonster.com">TemplateMonster</a></p>
+    </div>
+  </div>
+</footer>
