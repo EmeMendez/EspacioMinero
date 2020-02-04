cfaf5889b3a8098f28eb64aaa538fc2476248131
moawistky9 <diego_esc_9@hotmail.es>
2020-02-03 22:35:02 -0300

    respansibidad

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,8 +2,8 @@
 <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
 <header class="section page-header">
 <!-- RD Navbar-->
-<div class="rd-navbar-wrap">
-    <nav class="rd-navbar rd-navbar-modern bg-dark" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-fixed" data-lg-device-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="10px" data-xl-stick-up-offset="10px" data-xxl-stick-up-offset="10px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
+<div class="rd-navbar-wrap" >
+    <nav class="rd-navbar rd-navbar-modern bg-dark" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-fixed" data-lg-device-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="10px" data-xl-stick-up-offset="10px" data-xxl-stick-up-offset="10px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true" style="height: auto;min-height: 40px;">
     <div class="rd-navbar-main">
         <!-- RD Navbar Panel-->
         <div class="rd-navbar-panel ">
@@ -12,7 +12,7 @@
         <!-- RD Navbar Brand-->
         <div class="rd-navbar-brand logo-espacio " > <a class="brand" href="{{route('home')}}"><img class="logoE" src="/images/logo_blanco.png" alt=""/></a></div>
         </div>
-        <div class="rd-navbar-nav-wrap">
+        <div class="rd-navbar-nav-wrap" style="padding-bottom: 20px;">
         <!-- RD Navbar Nav-->
         <ul class="rd-navbar-nav ">
         <li class="rd-nav-item  {{setActive('home')}}"><a class="rd-nav-link " href="{{route('home')}}" >INICIO</a>
