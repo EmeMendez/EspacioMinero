4b13389ae1be5b8b736f956ade60c4d25f8c0eaa
NicolasVillalobos <nicolasvillalobosrojo@gmail.com>
2020-03-09 16:39:15 -0300

    tag query has fixed

--- app/Http/Controllers/ProveedorTagController.php
+++ app/Http/Controllers/ProveedorTagController.php
@@ -8,7 +8,9 @@ use App\ProveedorTag;
 class ProveedorTagController extends Controller
 {
     public function getProveedorTags($proveedor_rut){
-        $proveedor_tags = ProveedorTag::where('proveedor_rut', $proveedor_rut)->get();
+        $proveedor_tags = ProveedorTag::join("tag","tag.id","=","proveedor_tag.tag_id")
+                                        ->select('tag.nombre as nombre')
+                                        ->where('proveedor_rut', $proveedor_rut)->get();
         return $proveedor_tags;
     } 
 }
