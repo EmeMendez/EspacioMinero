8a4f87c7b7c3b6f89736b33fbd862a7da6078b6a
melon <ememendeztabilo@gmail.com>
2020-03-18 04:53:45 -0300

    choice view was modified

--- resources/views/options.blade.php
+++ resources/views/options.blade.php
@@ -1,63 +1,35 @@
 @extends('layout')
 @section('content')
-
+  <div class="bg-light py-4">
+    <h3 class="text-center">¡Únete y haz tu busqueda más simple!</h3>
+  </div><!-- end bg-light -->
     <div class="container">
-
-        <div class="row mx-2 my-5">
-
-            <a class="ml-1 perso" href="{{route('proveedor.create')}}">
-                <div class="col-6 ml-5 cont">
-                    <div>
-                        
-                            <img src="/images/provider.png">
-                        
-                    </div>
-                    <div>
-                        <h4>Soy empresa(proveedor)</h4>
-                    </div>
-            
-                </div>
-            </a>
-
-            <a class="" href="{{route('guest.create')}}">
-                <div class="col-6 ml-5 cont ">
-                    <div>
-                        
-                            <img src="/images/users.png">
-                        
-                    </div>
-                    <div>
-                        <h4>Soy persona natural</h4>
-                    </div>
-                    
-                </div>
-            </a>
-        
-        
-        
-        </div>
-    </div>
-
-<style>
-
-    .perso{
-        width: 40%;
-        
-    }
-
-    .cont{
-        border-radius: 15px;
-        justify-content: center;
-    }
-
-    img{
-        width: 100%;
-        height: 100%;
-    }
-
-    *{
-        box-sizing: inherit;
-    }
-</style>
-
-@endsection
+        <div class="row my-5">
+                <div class="col-6 text-center">
+                    <a  class="img-hover-zoom" href="{{route('proveedor.create')}}"> 
+                        <img class="w-50" src="/images/provider.png">   
+                    </a> 
+                    <h4 class="font-weight-bold my-3">Empresas</h4>
+                    <h6>(Proveedores)</h6>
+                </div><!-- end col-6 -->
+                <div class="col-6 text-center">
+                    <a  class="img-hover-zoom" href="{{route('guest.create')}}">
+                        <img class="w-50" src="/images/users.png">
+                    </a>
+                    <h4 class="font-weight-bold my-3">Personas</h4>
+                </div><!-- end col-6 -->
+        </div><!-- end row -->
+    </div><!-- end container -->
+    <style>
+        .img-hover-zoom {
+        overflow: hidden; 
+        }
+        .img-hover-zoom img {
+        transition: transform .5s ease;
+        }
+        /* [3] Finally, transforming the image when container gets hovered */
+        .img-hover-zoom:hover img {
+        transform: scale(1.1);
+        }
+    </style>
+ @endsection
